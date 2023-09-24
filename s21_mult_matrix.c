1ecfbd9b0984776dcf8dcedc864233ac7ec427e8
Андрей Аксюта (Jhinelia) <4epenaxa@mail.ru>
2023-09-24 23:14:29 +0300

    1.3
    Cleanup

--- s21_mult_matrix.c
+++ s21_mult_matrix.c
@@ -3,9 +3,7 @@
 int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
   if (incorrect_matrix(A) || incorrect_matrix(B)) return ERROR_MATR;
   if (A->columns != B->rows)
-    return ERROR_CALC;  // Matrices cannot be multiplied
-  // if (s21_create_matrix(A->rows, B->columns, result) != OK)return ERROR_MATR;
-  // // Error creating result matrix
+    return ERROR_CALC;
   s21_create_matrix(A->rows, B->columns, result);
   for (int i = 0; i < A->rows; i++) {
     for (int j = 0; j < B->columns; j++) {
