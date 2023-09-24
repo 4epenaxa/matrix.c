f92cc5e2978a53f940edc84a43b858d02a071e93
Андрей Аксюта (Jhinelia) <4epenaxa@mail.ru>
2023-09-24 23:15:33 +0300

    1.3.2
    Cleanup

--- s21_mult_number.c
+++ s21_mult_number.c
@@ -2,8 +2,6 @@
 
 int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
   if (incorrect_matrix(A)) return ERROR_MATR;
-  // if (s21_create_matrix(A->rows, A->columns, result) != OK) return
-  // ERROR_MATR; // Error creating result matrix
   s21_create_matrix(A->rows, A->columns, result);
   for (int i = 0; i < A->rows; i++)
     for (int j = 0; j < A->columns; j++)
